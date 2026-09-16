54f5e256ea15c318aa4c2837c59b00bcb8154697
clint zold <214383878+clintzold@users.noreply.github.com>
2026-09-16 06:52:06 -0600

    start mocking sections and add github deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://clintzold.github.io',
+  base: '/zold-digital'
+});
